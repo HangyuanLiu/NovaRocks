5985b93b2d60b62fc878999145567521e2aa64a2
Harbor Liu <460660596@qq.com>
2026-06-16 08:38:55 +0800

    fix(codegen): default partition_limit/topn_type at IR test sites (post-rebase integration with #322)

--- src/sql/codegen/ir/equiv.rs
+++ src/sql/codegen/ir/equiv.rs
@@ -799,6 +799,8 @@ mod tests {
                     nulls_first: false,
                 }],
                 analytic_partition_exprs: vec![],
+                partition_limit: None,
+                topn_type: None,
             }),
             vec![child],
             output_columns,
