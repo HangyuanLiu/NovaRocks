437e433347763be79fe7103f13d0bf356dc7a957
Harbor Liu <460660596@qq.com>
2026-07-03 09:51:54 +0800

    Fix chunk hydrate import ordering

--- src/exec/chunk/mod.rs
+++ src/exec/chunk/mod.rs
@@ -8,7 +8,7 @@ mod tests;
 pub(crate) mod type_compatibility;
 
 pub use chunk_impl::Chunk;
-pub(crate) use hydrate::hydrate_dictionary_columns_except;
 pub(crate) use hydrate::assert_no_dictionary;
+pub(crate) use hydrate::hydrate_dictionary_columns_except;
 pub use memory::record_batch_bytes;
 pub use schema::{ChunkFieldSchema, ChunkSchema, ChunkSchemaRef, ChunkSlotSchema};
