bf90e63a08a3653e9b022545e818a0a59720b29b
Harbor Liu <460660596@qq.com>
2026-07-31 13:44:00 +0800

    test(iceberg): cover static predicate rollback

--- sql-tests/iceberg/sql/iceberg_scan_pruning_correctness.sql
+++ sql-tests/iceberg/sql/iceberg_scan_pruning_correctness.sql
@@ -34,16 +34,30 @@ INSERT INTO ${case_db}.t_scan_pruning VALUES
   (13, 12, 'neighbor');
 
 -- query 2
+-- @skip_result_check=true
+SET enable_connector_static_predicate_pushdown = false;
+
+-- query 3
 SELECT id, part_id, payload
 FROM ${case_db}.t_scan_pruning
 WHERE part_id = 12 AND id = 12
 ORDER BY id;
 
--- query 3
+-- query 4
+-- @skip_result_check=true
+SET enable_connector_static_predicate_pushdown = true;
+
+-- query 5
+SELECT id, part_id, payload
+FROM ${case_db}.t_scan_pruning
+WHERE part_id = 12 AND id = 12
+ORDER BY id;
+
+-- query 6
 SELECT COUNT(*) AS cnt
 FROM ${case_db}.t_scan_pruning
 WHERE part_id = 99 AND id = 99;
 
--- query 4
+-- query 7
 -- @skip_result_check=true
 DROP TABLE ${case_db}.t_scan_pruning FORCE;
